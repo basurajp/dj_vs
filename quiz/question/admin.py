948af13d7d650a0dc9b1d0fcd53333063fb88f26
Basuraj <basurajp135@gmail.com>
2023-11-25 20:48:03 +0530

    orm done on 25  nov

--- quiz/question/admin.py
+++ quiz/question/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from question.models import Question
+from question.models import Option
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Option)
--- quiz/question/models.py
+++ quiz/question/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Question(models.Model):
     question = models.CharField(max_length=100)
-    # question = models.CharField(max_length=100,unique=True)---used to eliminate duplicates
     category = models.CharField(max_length=100)
-    # category = models.CharField(max_length=100,blank=True,Null=True)
-    # category = models.CharField(max_length=100,default='others')---default option
     added_dt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.question
+    
+    
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, related_name='options', on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
